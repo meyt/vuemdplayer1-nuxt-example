f5acdf75666eae7a9ced9318b7f0874ddcee3584
meyt <pasd3000@gmail.com>
2023-10-09 19:14:19 +0330

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# vuemdplayer1-example
+
+[vue-md-player@1](https://github.com/meyt/vue-md-player/tree/v1) example (nuxt@2 + vue@2)
+
+```bash
+# install
+yarn
+
+# serve with hot reload at localhost:3000
+yarn dev
+
+# generate static project
+yarn generate
+```
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <audio-player src="/audio.mp3" />
+    <video-player src="/video.mp4" />
+  </div>
+</template>
+
+<script>
+import { audioPlayer, videoPlayer } from 'vue-md-player'
+import 'vue-md-player/dist/vue-md-player.css'
+export default {
+  components: {
+    audioPlayer,
+    videoPlayer
+  }
+}
+</script>
+
+<style>
+  .container {
+    max-width: 600px;
+    margin: auto;
+  }
+  .vuemdplayer {
+    margin-bottom: 16px;
+  }
+</style>
